23717da337cc7e179b067e943ef2ce2583d0d556
olikellar <65731145+olikellar@users.noreply.github.com>
2020-05-22 11:26:19 +0100

    Add files via upload

---
+++ Learning_ResizingCanvas/sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+  if (windowWidth > windowHeight) {
+    createCanvas(windowHeight - 5, windowHeight - 5);
+  }
+  if (windowWidth < windowHeight) {
+    createCanvas(windowWidth - 5, windowWidth - 5);
+  }
+}
+
+function windowResized() {
+  if (windowWidth > windowHeight - 5) {
+    resizeCanvas(windowHeight - 5, windowHeight - 5)
+  }
+  if (windowWidth + 5 < windowHeight) {
+    resizeCanvas(windowWidth - 5, windowWidth - 5)
+  }
+}
+
+function draw() {
+  background(220);
+}
